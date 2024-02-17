9332ed5ee5f82d67978c712f4aa35d143c48fdc3
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-17 15:51:04 +0300

    ...

---
+++ FoodyTekmerWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+    }
+}
