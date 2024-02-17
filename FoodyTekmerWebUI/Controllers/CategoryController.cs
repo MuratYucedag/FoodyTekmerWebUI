724cfd63ee7defbc1a9bc67ff5a62f3126e9938a
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-17 14:02:23 +0300

    ....

---
+++ FoodyTekmerWebUI/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using FoodyTekmerDataAccessLayer.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        FoodyContext context = new FoodyContext();
+        public IActionResult Index()
+        {
+            var values = context.Categories.ToList();
+            return View(values);
+        }
+    }
+}
