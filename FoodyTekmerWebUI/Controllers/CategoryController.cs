7e00961cfbf647e88e88a3ee6a210db6178484c7
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-17 15:23:57 +0300

    ...

--- FoodyTekmerWebUI/Controllers/CategoryController.cs
+++ FoodyTekmerWebUI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using FoodyTekmerDataAccessLayer.Context;
+using FoodyTekmerEntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FoodyTekmerWebUI.Controllers
@@ -10,6 +11,43 @@ namespace FoodyTekmerWebUI.Controllers
         {
             var values = context.Categories.ToList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateCategory(Category p)
+        {
+            context.Categories.Add(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            context.Categories.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateCategory(int id)
+        {
+            var value = context.Categories.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateCategory(Category p)
+        {
+            context.Categories.Update(p);
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
