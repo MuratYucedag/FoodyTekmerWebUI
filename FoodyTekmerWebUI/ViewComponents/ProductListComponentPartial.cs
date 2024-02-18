d5dce03147b0c00957c053dea2a53beb047de46e
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-18 14:34:29 +0300

    ...

--- FoodyTekmerWebUI/ViewComponents/ProductListComponentPartial.cs
+++ FoodyTekmerWebUI/ViewComponents/ProductListComponentPartial.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FoodyTekmerBusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FoodyTekmerWebUI.ViewComponents
 {
     public class ProductListComponentPartial:ViewComponent
     {
+        private readonly IProductService _productService;
+        public ProductListComponentPartial(IProductService productService)
+        {
+            _productService = productService;
+        }
         public IViewComponentResult Invoke()
         {
-            return View();  
+            var values = _productService.TGetAllList();
+            return View(values);  
         }
     }
 }
