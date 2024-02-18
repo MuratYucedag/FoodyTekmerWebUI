e976d81a687d343a33de5121043a307f38f8863d
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-18 11:05:11 +0300

    ...

---
+++ FoodyTekmerWebUI/ViewComponents/FooterComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodyTekmerWebUI.ViewComponents
+{
+    public class FooterComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
