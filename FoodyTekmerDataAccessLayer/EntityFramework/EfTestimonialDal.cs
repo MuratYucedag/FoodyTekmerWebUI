15f4eeec721ed0b3b433e12a3bf1f3e8c9272bc0
MuratYucedag <yucedagmurat23@gmail.com>
2024-02-18 11:58:46 +0300

    ...

---
+++ FoodyTekmerDataAccessLayer/EntityFramework/EfTestimonialDal.cs
@@ -0,0 +1,15 @@
+﻿using FoodyTekmerDataAccessLayer.Abstract;
+using FoodyTekmerDataAccessLayer.Repositories;
+using FoodyTekmerEntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodyTekmerDataAccessLayer.EntityFramework
+{
+    public class EfTestimonialDal : GenericRepository<Testimonial>, ITestimonialDal
+    {
+    }
+}
